e3c3a099e594b520a4fe3cd79b48182c49786ca8
marunouchiR5 <53039597+marunouchiR5@users.noreply.github.com>
2023-12-02 23:06:29 -0500

    added profile image change logic

--- Assets/Scripts/UI/GameScreens/PlayerProfileDisplay.cs
+++ Assets/Scripts/UI/GameScreens/PlayerProfileDisplay.cs
@@ -11,12 +11,12 @@ public class PlayerProfileDisplay : HUD
 
     private void OnEnable()
     {
-        
+        GameStateManager.SanityChanged += OnPlayerProfileChanged;
     }
 
     private void OnDisable()
     {
-        
+        GameStateManager.SanityChanged -= OnPlayerProfileChanged;
     }
 
     protected override void SetVisualElements()
@@ -25,10 +25,46 @@ public class PlayerProfileDisplay : HUD
         m_PlayerProfile = m_Screen.Q(k_PlayerProfile);
     }
 
-    // event-handling methods
-    private void OnPlayerProfileChanged(string profileNumber)
+    // Event-handling method
+    private void OnPlayerProfileChanged()
     {
-        // todo
-        Debug.Log(profileNumber);
+        int currentSanity = GameStateManager.Instance.CurrentSanity;
+        string profileImage = DetermineProfileImage(currentSanity);
+        UpdatePlayerProfileImage(profileImage);
+    }
+
+    private string DetermineProfileImage(int currentSanity)
+    {
+        // Adjust the returned strings to match the names of your actual image assets
+        switch (currentSanity)
+        {
+            case 4:
+                return "unaware"; // Sanity level 4 - Normal state
+            case 3:
+                return "rabbitear"; // Sanity level 3 - Slightly stressed
+            case 2:
+                return "rabbitear"; // Sanity level 2 - Stressed
+            case 1:
+                return "rabbitear"; // Sanity level 1 - Very stressed
+            default:
+                return "unaware"; // In case of an unexpected value
+        }
+    }
+
+    private void UpdatePlayerProfileImage(string imageName)
+    {
+        // Assuming you have a method to fetch the correct image based on imageName
+        Texture2D profileTexture = FetchProfileImage(imageName);
+        if (profileTexture != null)
+        {
+            Debug.Log("Setting profile texture.");
+            m_PlayerProfile.style.backgroundImage = new StyleBackground(profileTexture);
+        }
+    }
+
+    private Texture2D FetchProfileImage(string imageName)
+    {
+        // Fetch the texture from your resources
+        return Resources.Load<Texture2D>($"UI/Textures/elements/{imageName}");
     }
 }
