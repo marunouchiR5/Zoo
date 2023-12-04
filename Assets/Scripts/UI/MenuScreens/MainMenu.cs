dab7c81f91285a7c42996d86cf5d26da50f1a5ea
marunouchiR5 <53039597+marunouchiR5@users.noreply.github.com>
2023-12-04 17:51:40 -0500

    added slideshow effect to main menu

--- Assets/Scripts/UI/MenuScreens/MainMenu.cs
+++ Assets/Scripts/UI/MenuScreens/MainMenu.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
 
 public class MainMenuUI : MonoBehaviour
 {
@@ -12,6 +14,9 @@ public class MainMenuUI : MonoBehaviour
 
     private ScrollView logScrollView; // ListView
     private Button backButton; // BackButton
+
+    private List<Texture2D> backgroundTextures;
+    private VisualElement backgroundElement;
 
     private RuleSet instructionsRuleSet;
 
@@ -50,6 +55,13 @@ public class MainMenuUI : MonoBehaviour
         {
             Debug.LogError("Instructions RuleSet not found in Resources/GameData/Instructions/Game Instruction");
         }
+
+        // Load background textures
+        backgroundTextures = new List<Texture2D>(Resources.LoadAll<Texture2D>("UI/Textures/background"));
+        backgroundElement = uiDocument.rootVisualElement.Q<VisualElement>("background");
+
+        // Start the slideshow coroutine
+        StartCoroutine(BackgroundSlideshow());
     }
 
     private void OnNewGameClicked()
@@ -138,6 +150,58 @@ public class MainMenuUI : MonoBehaviour
         {
             Debug.LogError("No RuleSet provided for instructions");
         }
+    }
+
+    private IEnumerator BackgroundSlideshow()
+    {
+        System.Random random = new System.Random();
+        int lastIndex = -1; // Variable to store the index of the last selected texture
+        while (true)
+        {
+            if (backgroundTextures.Count > 1) // Check if there are at least two textures
+            {
+                int index;
+                do
+                {
+                    // Randomly select a texture, different from the last one
+                    index = random.Next(backgroundTextures.Count);
+                } while (index == lastIndex);
+
+                // Update lastIndex for the next cycle
+                lastIndex = index;
+
+                Texture2D currentTexture = backgroundTextures[index];
+                backgroundElement.style.backgroundImage = new StyleBackground(currentTexture);
+
+                // Set initial alpha to 0
+                backgroundElement.style.opacity = 0;
+
+                // Fade in, hold, and fade out sequence
+                yield return StartCoroutine(FadeBackground(0f, 1f, 1f));
+                yield return new WaitForSeconds(1f);
+                yield return StartCoroutine(FadeBackground(1f, 0f, 1f));
+            }
+            else
+            {
+                // If only one texture is available, handle accordingly
+                // Possibly skip the slideshow or repeat the same image
+            }
+        }
+    }
+
+    private IEnumerator FadeBackground(float start, float end, float duration)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(start, end, elapsed / duration);
+            backgroundElement.style.opacity = alpha;
+            yield return null;
+        }
+
+        backgroundElement.style.opacity = end;
     }
 
     private void OnDestroy()
